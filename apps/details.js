5ae1ec1dbdbeb51aa2eeed803cfe85c3802c3832
Default <iraklibilashvili99@gmail.com>
2026-03-05 02:40:56 +0400

    დსადასხა

--- apps/details.js
+++ apps/details.js
@@ -1,4 +1,9 @@
 let main = document.querySelector(`main`);
 let id = window.location.search.split("=")[1]
 
-
+fetch(`https://rentcar.stepprojects.ge/api/Car/id/${id}`)
+.then(resp => resp.json())
+.then(data => {
+    console.log(data);
+    
+})
